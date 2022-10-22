3b19122485c8609c124d9fded265905b8a188e9c
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-10-22 16:53:51 +0300

    Update Program.cs

--- 6_Collections2/Program.cs
+++ 6_Collections2/Program.cs
@@ -8,17 +8,12 @@ namespace _6_Collections2
         static void Main(string[] args)
         {
             int myMoneyAccount = 0;
-            int purchaseAmount = 700; 
-            int nextPurchaseAmount = 300;
-            int middlePurchaseAmount = 500;
-            int penultimatePurchaseAmount = 500;
-            int lastPurchaseAmount = 1000;
             Queue<int> listPrices = new Queue<int>();
-            listPrices.Enqueue(purchaseAmount);
-            listPrices.Enqueue(nextPurchaseAmount);
-            listPrices.Enqueue(middlePurchaseAmount);
-            listPrices.Enqueue(penultimatePurchaseAmount);
-            listPrices.Enqueue(lastPurchaseAmount);
+            listPrices.Enqueue(700);
+            listPrices.Enqueue(300);
+            listPrices.Enqueue(500);
+            listPrices.Enqueue(500);
+            listPrices.Enqueue(1000);
 
             while (listPrices.Count > 0)
             {
