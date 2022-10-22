07188b09801eaa591c09ec4d39086f9ffade726e
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-10-22 19:42:05 +0300

    Update Program.cs

--- 6_Collections2/Program.cs
+++ 6_Collections2/Program.cs
@@ -7,27 +7,27 @@ namespace _6_Collections2
     {
         static void Main(string[] args)
         {
-            int myMoneyAccount = 0;
-            Queue<int> listPrices = new Queue<int>();
-            listPrices.Enqueue(700);
-            listPrices.Enqueue(300);
-            listPrices.Enqueue(500);
-            listPrices.Enqueue(500);
-            listPrices.Enqueue(1000);
+            int moneyAccount = 0;
+            Queue<int> prices = new Queue<int>();
+            prices.Enqueue(700);
+            prices.Enqueue(300);
+            prices.Enqueue(500);
+            prices.Enqueue(500);
+            prices.Enqueue(1000);
 
-            while (listPrices.Count > 0)
+            while (prices.Count > 0)
             {
                 Console.Clear();
                 Console.WriteLine("Суммы покупок: ");
 
-                foreach (var customer in listPrices)
+                foreach (var customer in prices)
                 {
                     Console.WriteLine(customer);
                 }
 
-                myMoneyAccount += listPrices.Dequeue();
+                moneyAccount += prices.Dequeue();
 
-                Console.WriteLine($"У вас на счёте - " + myMoneyAccount + " рублей.");
+                Console.WriteLine($"У вас на счёте - " + moneyAccount + " рублей.");
                 Console.WriteLine("Для продолжения нажмите любую кнопку.");
                 Console.ReadKey();
             }
