a36ceb38401cc5416295cecc351f58394c224be0
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-10-22 15:34:36 +0300

    Update Program.cs

--- 6_Collections2/Program.cs
+++ 6_Collections2/Program.cs
@@ -1,73 +1,54 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace _6_Collections2
 {
-//У вас есть множество целых чисел.Каждое целое число - это сумма покупки.
-//Вам нужно обслуживать клиентов до тех пор, пока очередь не станет пуста.
-//После каждого обслуженного клиента деньги нужно добавлять на наш счёт и выводить его в консоль.
-//После обслуживания каждого клиента программа ожидает нажатия любой клавиши, после чего затирает консоль 
-//и по новой выводит всю информацию, только уже со следующим клиентом
     internal class Program
     {
         static void Main(string[] args)
-        {          
-            bool canExitApp = true;
-            int MyMoneyAccount = 0; //денежный счёт
-            int purchaseAmount1 = 1255; //сумма покупки
-            int purchaseAmount2 = 5000;
-            int purchaseAmount3 = 1234;
-            int purchaseAmount4 = 432;
-            int purchaseAmount5 = 666;
-            string userInput;
-            string name1 = "Николай";
-            string name2 = "Владимир";
-            string name3 = "Сергей";
-            string name4 = "Илья";
-            string name5 = "Иван";
-            Queue<string> customers = new Queue<string>();//клиенты
-            Queue<int> cashAccount = new Queue<int>(); //денежный счёт
+        {
+            int moneyAccount = 0;
+            string name = "Николай";
+            string nextName = "Владимир";
+            string middleName = "Сергей";
+            string penultimateName = "Илья";
+            string lastName = "Иван";
+            int purchaseAmount = 700; 
+            int nextPurchaseAmount = 300;
+            int middlePurchaseAmount = 500;
+            int penultimatePurchaseAmount = 500;
+            int lastPurchaseAmount = 1000;
+            Queue<string> customers = new Queue<string>();
+            Queue<int> cashAccount = new Queue<int>();
+            customers.Enqueue(name);
+            customers.Enqueue(nextName);
+            customers.Enqueue(middleName);
+            customers.Enqueue(penultimateName);
+            customers.Enqueue(lastName);
+            cashAccount.Enqueue(purchaseAmount);
+            cashAccount.Enqueue(nextPurchaseAmount);
+            cashAccount.Enqueue(middlePurchaseAmount);
+            cashAccount.Enqueue(penultimatePurchaseAmount);
+            cashAccount.Enqueue(lastPurchaseAmount);
 
-            customers.Enqueue(name1);
-            customers.Enqueue(name2);
-            customers.Enqueue(name3);
-            customers.Enqueue(name4);
-            customers.Enqueue(name5);
-            cashAccount.Enqueue(purchaseAmount1);
-            cashAccount.Enqueue(purchaseAmount2);
-            cashAccount.Enqueue(purchaseAmount3);
-            cashAccount.Enqueue(purchaseAmount4);
-            cashAccount.Enqueue(purchaseAmount5);
-
-            Console.WriteLine($"У вас на счёте - " + MyMoneyAccount + " рублей"); 
-            Console.WriteLine("\nСейчас в очереди стоят: ");
-
-            foreach (var customer in customers)
+            while (cashAccount.Count > 0)
             {
-                Console.WriteLine(customer);
-            }
+                Console.Clear();
+                Console.WriteLine("\nСейчас в очереди стоят: ");
 
-            Console.WriteLine("\nСейчас идёт - " + customers.Peek());
-            Console.WriteLine("К вам подошёл - " + customers.Dequeue());
-            Console.WriteLine("Он хочет купить товар на сумму - " + cashAccount.Peek());
-            Console.WriteLine("Он купил товар на сумму - " + cashAccount.Dequeue());
-            Console.WriteLine($"\nУ вас на счёте - " + MyMoneyAccount + " рублей");
-            Console.WriteLine("\nСейчас в очереди стоят: ");
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customer);
+                }
 
-            foreach (var customer in customers)
-            {
-                Console.WriteLine(customer);
-            }
+                Console.WriteLine("\nК вам подошёл - " + customers.Dequeue());
+                Console.WriteLine("Он купил товаров на сумму " + cashAccount.Peek() + " рублей.");
 
-            Console.WriteLine("\nСейчас идёт - " + customers.Peek());
-            Console.WriteLine("К вам подошёл - " + customers.Dequeue());
-            Console.WriteLine("Он купил товаров на сумму - " + cashAccount.Dequeue());
-            Console.WriteLine($"\nУ вас на счёте - " + MyMoneyAccount + " рублей");
-            Console.WriteLine("\nСейчас в очереди стоят: ");
+                moneyAccount += cashAccount.Dequeue();
 
-            foreach (var customer in customers)
-            {
-                Console.WriteLine(customer);
+                Console.WriteLine($"У вас на счёте - " + moneyAccount + " рублей.");
+                Console.WriteLine("Для продолжения нажмите любую кнопку.");
+                Console.ReadKey();
             }
         }
     }
